fe2c7a4975f80c9bbf9c974ae821e5e09da504ff
Azooz-Almutiri <bdalzyzalmtyry629@gmail.com>
2026-08-15 06:46:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def home():
     return "Bot is alive!"
 
 def run():
-    # استخدام البورت المخصص من Render تلقائياً
+    # سحب البورت المخصص من Render
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
 
